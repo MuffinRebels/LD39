1f1a67e4dd3eebaf0e9190fffdef24e2f1b92f51
Pixels <timdewit@outlook.com>
2017-07-29 04:28:59 +0200

    Wrote basics for player
    Meh

--- Putential Outbreak/Assets/Scripts/Player.cs
+++ Putential Outbreak/Assets/Scripts/Player.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
+    public const string PLAYER_NAME = "Putin";
+
+    [SerializeField]
+    private Vector2 _speed;
+
+    [SerializeField]
+    private int _maxHealth = 100;
+
+    [SerializeField]
+    private int _maxOxygen = 100;
+
+    private int _health;
+    private int _oxygen;
+
 	void Start () {
-		
-	}
+        _health = _maxHealth;
+        _oxygen = _maxOxygen;
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 	}
+
+    public int GetHealth()
+    {
+        return _health;
+    }
+
+    public int GetMaxHealth()
+    {
+        return _maxHealth;
+    }
+
+    public void ModifyHealth(int newVal)
+    {
+        _health = (int)Mathf.Clamp(_health + newVal, 0, _maxHealth);
+        if (_health == 0)
+            Die();
+    }
+
+    private void Die()
+    {
+        // TODO: Implement Die() function
+    }
+
 }
